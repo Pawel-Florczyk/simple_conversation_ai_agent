3af71c920572efbd0a7d8266bb284e76345df0da
Pawel_Florczyk <pawelf775@gmail.com>
2026-05-13 18:01:42 +0200

    refactor the agent

--- main.py
+++ main.py
@@ -1,59 +1,32 @@
 import os
+from typing import List, Any
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage, BaseMessage
 
 load_dotenv()
 
-os.environ["GOOGLE_API_KEY"] = "PASTE YOUR API KEY"
+os.environ["GOOGLE_API_KEY"] = "INSERT YOUR GOOGLE API KEY HERE"
 
-def run_simple_agent():
-    api_key = os.getenv("GOOGLE_API_KEY")
-    if not api_key:
-        print("Error: GOOGLE_API_KEY not set.")
-        return
 
-    try:
-        llm = ChatGoogleGenerativeAI(
-            model="gemini-2.5-flash",
-            temperature=0.5
-        )
+class Agent:
+    def __init__(self, model_name="gemini-2.5-flash", temperature=0.5):
+        self.llm = ChatGoogleGenerativeAI(model=model_name, temperature=temperature)
+        self.system_instruction = SystemMessage(
+            content="You have to give an answers on questions. You can answer in Polish or English.")
+        self.memory: List[BaseMessage] = [self.system_instruction]
 
-        history = []
+    def ask(self, user_text: str) -> str | list[Any]:
+        self.memory.append(HumanMessage(content=user_text))
+        response = self.llm.invoke(self.memory)
+        self.memory.append(AIMessage(content=response.content))
+        return response.content
 
-        print("Simple agent powered by Google Gemini has already started.")
-        system_instruction = SystemMessage(
-            content="You are simple AI assistant. "
-                    "Your name is Simple Agent. "
-                    "You have to answer in English or Polish,"
-                    "and remember the context of conversation."
-        )
-
-        while True:
-            user_text = input("Ask Agent something: ")
-
-            if user_text.lower() in ["bye", "czesc", "do widzenia"]:
-                print("Agent: Bye!")
-                break
-
-            if not user_text.strip():
-                continue
-
-            history.append(HumanMessage(content=user_text))
-
-            response = llm.invoke([
-                system_instruction,
-                HumanMessage(content=user_text)
-            ])
-
-            history.append(AIMessage(content=response.content))
-
-            print(f"\nAgent: {response.content}\n")
-
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-        print("Make sure that your API is correct "
-              "and you have connection with internet.")
 
 if __name__ == "__main__":
-    run_simple_agent()
+    agent = Agent()
+    while True:
+        txt = input("Ask Agent something: ")
+        if txt.lower() in ["bye", "czesc", "do widzenia"]:
+            break
+        print(f"Agent: {agent.ask(txt)}")
