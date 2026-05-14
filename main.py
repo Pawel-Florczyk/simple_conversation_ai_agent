64a36bc81ae6e48b5b59ece2aed09b3e105f92a3
Pawel_Florczyk <pawelf775@gmail.com>
2026-05-14 14:21:02 +0200

    Added SQLite base

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import sqlite3
+from datetime import datetime
 from typing import List, Any
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
@@ -6,7 +8,7 @@ from langchain_core.messages import HumanMessage, SystemMessage, AIMessage, Base
 
 load_dotenv()
 
-os.environ["GOOGLE_API_KEY"] = "INSERT YOUR GOOGLE API KEY HERE"
+os.environ["GOOGLE_API_KEY"] = "INSERT YOUR API KEY HERE"
 
 
 class Agent:
@@ -17,14 +19,26 @@ class Agent:
                     "You can answer in Polish or English."
         )
         self.memory: List[BaseMessage] = [self.system_instruction]
+        self.conn = sqlite3.connect("agent_db_memory.sqlite")
+        self.cursor = self.conn.cursor()
+
+    def save_to_db(self, role: str, content: str | list[Any]) -> None:
+        timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        self.cursor.execute("INSERT INTO ai_agent_memory (role, content, timestamp) VALUES (?,?,?)", (role, content, timestamp))
+        self.conn.commit()
 
     def ask(self, user_text: str) -> str | list[Any]:
+        self.save_to_db("user", user_text)
+
         self.memory.append(HumanMessage(content=user_text))
 
         if len(self.memory) > 10:
             self.memory.pop(1)
 
         response = self.llm.invoke(self.memory)
+
+        self.save_to_db("assistant", response.content)
+
         self.memory.append(AIMessage(content=response.content))
         return response.content
 
